15d0ea5a4c1717bb3223725cd59ce62ef11ab5e4
witrdotnet <witr.net@gmail.com>
2018-07-16 20:24:43 +0200

    import angular-checklist module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ChecklistModule } from 'angular-checklist';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChecklistModule
   ],
   providers: [],
   bootstrap: [AppComponent]
