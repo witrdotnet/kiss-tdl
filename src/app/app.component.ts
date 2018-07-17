7c5175a37913476682e93b65327c0e7dff18baaa
witrdotnet <witr.net@gmail.com>
2018-07-17 06:34:37 +0200

    logo kiss-tdl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'KISS To-Do-List';
   options = [
 	{text: "a", value: "1"},
 	{text : "b", value: "2"}
